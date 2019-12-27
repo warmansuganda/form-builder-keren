d92ecb6ef24d1ff1234679bb44634ca13a258039
Warman Suganda <warman.suganda@gmail.com>
2019-12-27 18:13:04 +0700

    setup form builder

--- config/paths.js
+++ config/paths.js
@@ -73,6 +73,7 @@ module.exports = {
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
+  appBuilderJs: resolveModule(resolveApp, 'src/Builder'),
   appPackageJson: resolveApp('package.json'),
   appSrc: resolveApp('src'),
   appTsConfig: resolveApp('tsconfig.json'),
--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import {FormBuilder} from './Builder';
 import './App.css';
+
+const items = [
+{
+  key: "Header",
+  name: "Header Text",
+  icon: "fa fa-header"
+},
+{
+  key: "Paragraph",
+  name: "Paragraph",
+  icon: "fa fa-paragraph"
+},
+{
+  key: "Dropdown",
+  name: "Dropdown",
+  icon: "fa fa-caret-square-o-down"
+}
+];
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <FormBuilder
+                 items={items}
+                 onSubmit={() => {}}
+               />
     </div>
   );
 }
